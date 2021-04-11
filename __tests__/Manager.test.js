abf45c371b6b8c0e7d9611d8c19c54ef6b85889c
Vanessa G <vlopez_jv@hotmail.com>
2021-04-10 22:01:30 -0700

    pseudocoded reqs for libs

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,20 @@
+
+// name
+
+// id
+
+// email
+
+// getName()
+
+// getId()
+
+// getEmail()
+
+// getRole()
+
+// In addition to Employee's properties and methods, Manager will also have:
+
+// officeNumber
+
+// getRole() // Overridden to return 'Manager'
